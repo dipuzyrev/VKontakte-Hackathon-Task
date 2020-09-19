0cea54ad9cb14d39afda72c0657e1f5ac4cd058b
Dmitry Puzyrev <puzyrev.dmitry@gmail.com>
2020-09-19 10:46:33 +0300

    Updates

---
+++ precache-manifest.a21c5c73f37fdadf066ae170610daebc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca773a57f242678e9a3816df9e21be9",
+    "url": "/vk-hack-emotions-web/index.html"
+  },
+  {
+    "revision": "7e37f5a440e72bb44922",
+    "url": "/vk-hack-emotions-web/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "40302380726ebf141ead",
+    "url": "/vk-hack-emotions-web/static/css/main.7ffd9f21.chunk.css"
+  },
+  {
+    "revision": "7e37f5a440e72bb44922",
+    "url": "/vk-hack-emotions-web/static/js/2.cf0aa949.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/vk-hack-emotions-web/static/js/2.cf0aa949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40302380726ebf141ead",
+    "url": "/vk-hack-emotions-web/static/js/main.a7ab0bec.chunk.js"
+  },
+  {
+    "revision": "0cc67d3408a8e4b49f32",
+    "url": "/vk-hack-emotions-web/static/js/runtime-main.dce31221.js"
+  }
+]);
